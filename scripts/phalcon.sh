507f8ff9586606ebbd156132f9e6314f555a8061
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-01-26 23:00:39 -0200

    Update phalcon.sh

--- scripts/phalcon.sh
+++ scripts/phalcon.sh
@@ -7,20 +7,3 @@ if [ ! -e "/etc/php5/fpm/conf.d/20-phalcon.ini" ]; then
     sudo apt-get update
     sudo apt-get install -y php5-phalcon
 fi
-
-# VERSION="2.0.8"
-# sudo apt-get install -y php5-dev php5-mysql gcc libpcre3-dev
-
-# mkdir /home/vagrant/phalcon
-# cd /home/vagrant/phalcon
-# git clone git://github.com/phalcon/cphalcon.git
-# cd cphalcon
-# git checkout tags/phalcon-v$VERSION
-# cd build
-# sudo ./install
-
-# sudo echo "extension=phalcon.so" >> /etc/php5/fpm/conf.d/20-phalcon.ini
-# sudo echo "extension=phalcon.so" >> /etc/php5/cli/conf.d/20-phalcon.ini
-
-# sudo echo extension=phalcon.so > /etc/php5/fpm/conf.d/phalcon.ini
-# sudo php5enmod phalcon
