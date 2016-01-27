d41bf846ab3b9063424c63d43102f988330723a3
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-01-26 23:04:26 -0200

    Update mysql.sh

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -5,7 +5,9 @@ CONFIG_PROVISION_PATH="/temp/config"
 
 echo "Installing MySQL..."
 
-apt-get install debconf-utils -y > /dev/null
+apt-get install -y debconf-utils > /dev/null
+
 echo "mysql-server mysql-server/root_password password $DB_PASSWORD" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $DB_PASSWORD" | debconf-set-selections
+
 apt-get install -y mysql-server
