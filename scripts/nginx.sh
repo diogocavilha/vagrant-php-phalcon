1cd04a8aaf55c84664b2a83d4c2f3d9a914ab3b2
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-02-03 01:53:23 -0200

    Improve nginx configuration

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -8,20 +8,12 @@ apt-get install -y nginx
 
 echo "Importing Nginx configuration..."
 
-sudo rm -rf /etc/nginx/sites-enabled/*.* > /dev/null
 sudo rm -rf /etc/nginx/sites-available/*.* > /dev/null
-sudo rm -rf /etc/nginx/sites-enabled/* > /dev/null
 sudo rm -rf /etc/nginx/sites-available/* > /dev/null
+sudo rm -rf /etc/nginx/sites-enabled/*.* > /dev/null
+sudo rm -rf /etc/nginx/sites-enabled/* > /dev/null
 sudo rm -rf /etc/nginx/conf.d/*.* > /dev/null
 sudo rm -rf /etc/nginx/conf.d/* > /dev/null
 
-sudo cp -ivf $CONFIG_PROVISION_PATH/nginx/serverblock_default.conf /etc/nginx/conf.d/default.conf
-chmod 644 /etc/nginx/conf.d/default.conf
-
-if [ "$(ls -A $CONFIG_PROVISION_PATH/nginx/serverblocks)" ]; then
-    serverblocks=$(ls $CONFIG_PROVISION_PATH/nginx/serverblocks)
-    for serverblock in $serverblocks
-    do
-        sudo cp -ivf $CONFIG_PROVISION_PATH/nginx/serverblocks/$serverblock /etc/nginx/conf.d/$serverblock
-    done
-fi
+sudo cp -iv $CONFIG_PROVISION_PATH/nginx/conf.d/* /etc/nginx/conf.d/
+sudo chmod 644 /etc/nginx/conf.d/default.conf
