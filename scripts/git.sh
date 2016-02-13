602862b25cbc8730b0aa39231054466d9715768e
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-02-13 19:47:18 -0200

    Improve git configuration.

--- scripts/git.sh
+++ scripts/git.sh
@@ -8,7 +8,4 @@ apt-get install -y git
 
 echo "Configuring GIT..."
 
-if [ -e "/home/vagrant/.gitconfig" ]; then
-    rm /home/vagrant/.gitconfig > /dev/null
-fi
-cp -iv $CONFIG_PROVISION_PATH/gitconfig /home/vagrant/.gitconfig
+cp -vf $CONFIG_PROVISION_PATH/gitconfig /home/vagrant/.gitconfig
