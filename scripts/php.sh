71ba592ab9698ba9063a48241784ea122a8b7c5a
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-04-01 23:14:39 -0300

    Update php.sh

--- scripts/php.sh
+++ scripts/php.sh
@@ -23,19 +23,17 @@ if [ ! -e "/etc/php5/fpm/conf.d/mcrypt.ini" ]; then
     cd /etc/php5/fpm/conf.d && sudo ln -s ../../mods-available/mcrypt.ini
 fi
 
-echo "Enabling PHP errors..."
-
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
-
 if [ ! -e "/usr/local/bin/composer" ]; then
     echo "Installing Composer..."
-    curl -sS https://getcomposer.org/installer | php
-    sudo cp -iv composer.phar /usr/local/bin/composer
-    rm -f composer.phar
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 fi
 sudo composer self-update
 
 echo "Configuring PHP FPM..."
 
 sed -i 's#listen = /var/run/php5-fpm.sock#listen = 127.0.0.1:9000#' /etc/php5/fpm/pool.d/www.conf
+
+echo "Enabling PHP errors..."
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/fpm/php.ini
