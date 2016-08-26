0a516763b46ac7d40a03817064d414cd9e1a7351
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-08-26 11:07:31 -0300

    Update phalcon.sh

--- scripts/phalcon.sh
+++ scripts/phalcon.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Installing Phalcon Framework..."
+if [ ! -e "/etc/php/7.0/fpm/conf.d/20-phalcon.ini" ]; then
+    echo "Installing Phalcon Framework..."
 
-curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | sudo bash
-sudo apt-get install -y php7.0-phalcon
+    curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh | sudo bash
+    sudo apt-get install -y php7.0-phalcon
+fi
