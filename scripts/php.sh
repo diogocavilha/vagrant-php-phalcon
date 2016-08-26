12d2046bdb6eafe2827b3d8809ca96ac64de071e
Diogo Alexsander Cavilha <diogocavilha@gmail.com>
2016-08-26 11:09:41 -0300

    Update php.sh

--- scripts/php.sh
+++ scripts/php.sh
@@ -9,13 +9,13 @@ sudo apt-get install -y --force-yes php7.0
 
 echo "Installing PHP extensions..."
 
-apt-get install -y --force-yes php7.1-cli
-apt-get install -y --force-yes php7.1-fpm
-apt-get install -y --force-yes php7.1-curl
-apt-get install -y --force-yes php7.1-intl
-apt-get install -y --force-yes php7.1-mcrypt
-apt-get install -y --force-yes php7.1-gd
-apt-get install -y --force-yes php7.1-mysql
+apt-get install -y --force-yes php7.0-cli
+apt-get install -y --force-yes php7.0-fpm
+apt-get install -y --force-yes php7.0-curl
+apt-get install -y --force-yes php7.0-intl
+apt-get install -y --force-yes php7.0-mcrypt
+apt-get install -y --force-yes php7.0-gd
+apt-get install -y --force-yes php7.0-mysql
 apt-get install -y --force-yes php-redis
 apt-get install -y --force-yes php-xdebug
 
@@ -29,9 +29,4 @@ sudo composer self-update
 
 echo "Configuring PHP FPM..."
 
-sed -i 's#listen = /run/php/php7.1-fpm.sock#listen = 127.0.0.1:9000#' /etc/php/7.1/fpm/pool.d/www.conf
-
-echo "Enabling PHP errors..."
-
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.1/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/fpm/php.ini
+sed -i 's#listen = /run/php/php7.0-fpm.sock#listen = 127.0.0.1:9000#' /etc/php/7.0/fpm/pool.d/www.conf
